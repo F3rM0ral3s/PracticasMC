5be6b1ec751318055eb7406ebc7daaa63497f876
markosh <marco.sanchez.h@proton.me>
2024-03-11 15:22:48 -0600

    P1: Definición de entradas y rutinas

--- control-leds/main.py
+++ control-leds/main.py
@@ -1,1 +1,34 @@
-from machine import Pin
+from machine import Pin, Timer
+
+r_led1 = Pin(8, Pin.OUT)
+g_led1 = Pin(9, Pin.OUT)
+o_led1 = Pin(10, Pin.OUT)
+b_led1 = Pin(11, Pin.OUT)
+b_led2 = Pin(12, Pin.OUT)
+o_led2 = Pin(13, Pin.OUT)
+g_led2 = Pin(14, Pin.OUT)
+r_led2 = Pin(15, Pin.OUT)
+
+def off():
+    return None
+
+def on_off():
+    return None
+
+def nibbles():
+    return None
+
+def half_nibbles_right():
+    return None
+
+def half_nibbles_left():
+    return None
+
+def shift_right():
+    return None
+
+def shift_left():
+    return None
+
+def zig_zag():
+    return None
